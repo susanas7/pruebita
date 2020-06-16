6ab9c5f0bad541f36701af6788983b548ca83883
Susana Santamaria <susana.s7@hotmail.com>
2020-06-15 19:03:05 -0500

    Estructurando el proyecto, login y registro

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,15 @@
 
 //('/nombre de la ruta q queremos crear', 'nombre del archivo a llamar(home.blade.php)
 //')->name es buena practica
-Route::view('/', 'home')->name('home');
+Route::view('/inicio', 'inicio')->name('inicio');
 Route::view('/about', 'about')->name('about');
 Route::view('/portfolio', 'portfolio')->name('portfolio');
 Route::view('/contact', 'contact')->name('contact');
 
-});
+Route::view('/home', 'home')->name('home');
+
+
+Auth::routes();
+
+Route::get('/', 'HomeController@index')->name('home');
 ?>
